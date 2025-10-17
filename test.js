922922901919fbb0643423a960a2b3835da7e7ec
filiphalonen <157317694+filiphalonen@users.noreply.github.com>
2025-10-17 17:12:24 +0200

    added colors + removed noLoop

--- test.js
+++ test.js
@@ -1,10 +1,20 @@
 function setup() {
   createCanvas(750, 750);
+  frameRate(5);
 }
 
 const size = 500;
 const layers = 50;
 const variance = size / 25;
+
+const palette = [
+  color(204, 16, 45),
+  color(232, 133, 20),
+  color(255, 255, 40),
+  color(125, 198, 44),
+  color(113, 192, 222),
+  color(99, 76, 138),
+];
 
 function getRandomValue(pos, variance) {
   return pos + map(Math.random(), 0, 1, -variance, variance);
@@ -32,7 +42,8 @@ function drawLayers(x, y, size, layers) {
   for (let i = 0; i < layers; i++) {
     if (Math.random() > 0.8) continue;
 
-    stroke(255);
+    stroke(random(palette));
+    strokeWeight(1.5);
 
     const s = (size / layers) * (i + 1);
     const v = variance * (i / layers);
@@ -44,6 +55,4 @@ function draw() {
   background(0);
 
   drawLayers(width / 2, height / 2, size, layers);
-
-  noLoop();
 }
